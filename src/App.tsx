d3a650eaa6205710a27705c61987ccf869002a7b
Vlad <juzopigeon@gmail.com>
2021-10-18 15:46:55 +0600

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import  { useState } from 'react'
+import { useQuery } from 'react-query';
+// Components
+import Drawer from '@material-ui/core/Drawer'
+import React from 'react';
+
+
+function App() {
+  return (
+   <div className="app">
+     Start
+   </div>
+  );
+}
+
+export default App;
